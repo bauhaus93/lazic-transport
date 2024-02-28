3a007ce818e7fbfbf2c726154c1a6a72d3ffe185
Jakob Fischer <schlemihl123+git@gmail.com>
2024-02-28 17:51:21 +0100

    Change build/deployment

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf www && \
-mkdir -p www && \
-./main.py src www && \
-sass --verbose --no-source-map src/scss/custom.scss www/bootstrap.css  && \
-cp -rv assets/* www/
-
-python3 -m http.server -d www
