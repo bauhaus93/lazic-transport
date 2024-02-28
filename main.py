a1279c38039d5fb57b3c73bbc668df7d1fc75720
Jakob Fischer <schlemihl123+git@gmail.com>
2024-02-28 18:07:59 +0100

    Remove encoding from python logging

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from context import CONTEXT
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s:  %(message)s",
-    encoding="utf-8",
     level=logging.INFO,
 )
 _logger = logging.getLogger(__name__)
